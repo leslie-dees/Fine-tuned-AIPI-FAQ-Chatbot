9a11f12e2984e6f6d88a40d239765578c04058ee
Leslie Dees <leslie.dees@duke.edu>
2024-04-16 15:18:00 -0400

    Added install requirements

--- src/main.py
+++ src/main.py
@@ -8,13 +8,13 @@ def main():
     """
     # Setup the CRAG pipeline
     pinecone_api_key = os.getenv('PINECONE_API_KEY')
-    travily_api_key = os.getenv('TRAVILY_API_KEY')
+    tavily_api_key = os.getenv('TAVILY_API_KEY')
     adapter_id = os.getenv('ADAPTER_ID')
     index = setup_index(pinecone_api_key)
     embeddings_model = setup_embeddings_model()
     pipe, tokenizer = setup_pipeline_and_tokenizer(adapter_id)
     grader = setup_grader(pipe)
-    web_search_agent = setup_web_search_agent(travily_api_key)
+    web_search_agent = setup_web_search_agent(tavily_api_key)
 
     # Example user question
     user_question = "What are some restaurants?"
